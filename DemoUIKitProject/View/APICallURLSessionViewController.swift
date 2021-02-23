7453864f82836b386e82d5070d9d52ce753da090
Prachi Bile <bileprachi@gmail.com>
2021-02-23 18:59:41 +0530

    Commit APICall VC

--- DemoUIKitProject/View/APICallURLSessionViewController.swift
+++ DemoUIKitProject/View/APICallURLSessionViewController.swift
@@ -3,7 +3,7 @@
 //  DemoUIKitProject
 //
 //  Created by Prachi Bile on 23/02/21.
-//
+//  Description: Call given API to fetch Author details and display in Collection View.
 
 import UIKit
 
@@ -14,16 +14,5 @@ class APICallURLSessionViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
