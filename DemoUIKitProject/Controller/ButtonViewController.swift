99ecacff20ab7dc99768db1752ea267f40de6f55
Prachi Bile <bileprachi@gmail.com>
2021-02-23 13:14:02 +0530

    Commit after file location change

---
+++ DemoUIKitProject/Controller/ButtonViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonViewController.swift
+//  DemoUIKitProject
+//
+//  Created by Prachi Bile on 27/01/21.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ DemoUIKitProject/Controller/LabelViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LabelViewController.swift
+//  DemoUIKitProject
+//
+//  Created by Prachi Bile on 26/01/21.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
